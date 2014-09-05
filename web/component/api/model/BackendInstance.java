5daeb81c5f72e23b61228279c182afa93759be44
Hiroshi1978 <h_kobayashi@kogasoftware.com>
2014-09-05 21:25:02 +0900

    Update BackendInstance.java

--- web/component/api/model/BackendInstance.java
+++ web/component/api/model/BackendInstance.java
@@ -18,14 +18,23 @@ public interface BackendInstance {
      * @return instance of LoadBalancer or null.
      */
     public LoadBalancer getLoadBalancer();
+
+    /**
+     * Returns List of all the load balancers with which this backend instance is registered.
+     * If this backend instance is not registered with any load balancer, this method returns empty list.
+     * @return list of load balacers with which this backend instance is registered.
+     */
+    public List<LoadBalancer> getLoadBalancers();
     public String getId();
-    public void register(LoadBalancer lb);
+
+    /*
+    * Register this backend instance with the specified load balancer.
+    */
+    public void registerWith(LoadBalancer lb);
     
     /*
-    * If this backend instance has already been registered with any load balancer,
-    * deregister from it and return the instance of the load balancer.
-    * If failed to deregister or not been registered, return null.
+    * Deregister this backend instance from the specified load balancer.
     */
-    public LoadBalancer deregister();
+    public LoadBalancer deregisterFrom(LoadBalancer lb);
     public BackendInstanceState getBackendInstanceState();
 }
