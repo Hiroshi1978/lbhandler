ff5a5b0929b59dd4003842d287b29cbd392c44c1
Hiroshi1978 <h_kobayashi@kogasoftware.com>
2014-09-19 08:05:02 +0900

    implment methods of VPCImpl class.

--- src/web/component/impl/aws/model/VPCImpl.java
+++ src/web/component/impl/aws/model/VPCImpl.java
@@ -12,36 +12,74 @@ import web.component.api.model.VPC;
  *
  * @author Hiroshi
  */
-public class VPCImpl implements VPC{
+public class VPCImpl extends AWSModelBase implements VPC{
 
+    private final com.amazonaws.services.ec2.model.Vpc ec2Vpc = new com.amazonaws.services.ec2.model.Vpc();
+
+    private VPCImpl(String id){
+        com.amazonaws.services.ec2.model.Vpc source = ec2().getExistEc2Vpc(id);
+        ec2Vpc.setCidrBlock(source.getCidrBlock());
+        ec2Vpc.setDhcpOptionsId(source.getDhcpOptionsId());
+        ec2Vpc.setInstanceTenancy(source.getInstanceTenancy());
+        ec2Vpc.setIsDefault(source.getIsDefault());
+        ec2Vpc.setTags(source.getTags());
+        ec2Vpc.setVpcId(source.getVpcId());
+    }
+    
     @Override
     public String getCidrBlock() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return ec2Vpc.getCidrBlock();
     }
 
     @Override
     public String getDhcpOptionsId() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return ec2Vpc.getDhcpOptionsId();
     }
 
     @Override
     public String getInstanceTenancy() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return ec2Vpc.getInstanceTenancy();
     }
 
     @Override
     public Boolean getIsDefault() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return ec2Vpc.getIsDefault();
     }
 
     @Override
     public String getState() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
+        String state = "Unknow state";
+        
+        try{
+            state = ec2().getExistEc2Vpc(getVpcId()).getState();
+        }catch(RuntimeException e){
+            //do nothing.
+        }
+        
+        return state;
     }
 
     @Override
     public String getVpcId() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return ec2Vpc.getVpcId();
     }
     
+    @Override
+    public boolean equals(Object toBeCompared){
+        if(toBeCompared instanceof VPCImpl)
+            if(getVpcId().equals(((VPC)toBeCompared).getVpcId()))
+                return true;
+        return false;
+    }
+    
+    @Override
+    public int hashCode(){
+        return 31 * ec2Vpc.hashCode();
+    }
+    
+    @Override
+    public String toString(){
+        return ec2Vpc.toString();
+    }
 }
