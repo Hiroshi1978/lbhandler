419bb25bfb16002f688bd455475123ce8c276871
Hiroshi1978 <h_kobayashi@kogasoftware.com>
2014-09-25 00:09:39 +0900

    delete unrequired import statements.

--- test/web/component/impl/aws/model/InstanceImplTest.java
+++ test/web/component/impl/aws/model/InstanceImplTest.java
@@ -6,11 +6,8 @@
 
 package web.component.impl.aws.model;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import org.junit.After;
 import org.junit.AfterClass;
 import static org.junit.Assert.*;
