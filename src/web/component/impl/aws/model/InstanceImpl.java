3768b3ae4707a713ac53921c527ef2ce9272d4ef
Hiroshi1978 <h_kobayashi@kogasoftware.com>
2014-09-28 10:12:49 +0900

    delete unrequired import statements.

--- src/web/component/impl/aws/model/InstanceImpl.java
+++ src/web/component/impl/aws/model/InstanceImpl.java
@@ -6,8 +6,6 @@
 
 package web.component.impl.aws.model;
 
-import com.amazonaws.services.elasticloadbalancing.model.LoadBalancerDescription;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
